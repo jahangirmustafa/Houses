5b5e39582d673de9dff8a6ae5cfbf64d4a2dbca4
jahangir mustafa <the.jahangir@gmail.com>
2017-10-21 12:36:18 +0500

    Initial commit

---
+++ Houses/Controllers/AgentProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Houses.Models;
+using System.Diagnostics;
+
+namespace Houses.Controllers
+{
+    public class AgentProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
